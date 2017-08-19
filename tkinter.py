294e4cd90b3afadd4b109bb58f516165176a7e86
AvidRP <s2pant@edu.uwaterloo.ca>
2017-08-19 15:21:35 -0600

    added buttons

--- tkinter.py
+++ tkinter.py
@@ -6,16 +6,31 @@ from Tkinter import *
 #obj creation from tk class store it in root
 root = Tk() #blank window
 
-#baisc text
-#first param is where you want the text to go
-#second is the text itself
-theLabel = Label(root, text = "This is some text")
-root.minsize(width="500", height="500")
+# root.minsize(width="400", height= "400")
 
-#to actually display the text
-#it just packs the text wherever it can
-theLabel.pack()
+#frames act like invisible containers
+topFrame = Frame(root)
+#to display frame: (anytime wanna display sth gotta pack
+#by default its packs at the top
+topFrame.pack()
+
+bottomFrame = Frame(root)
+#we wanna specify where we pack the bottomFrame in our window
+bottomFrame.pack(side=BOTTOM)
+
+#putting some widgets in there
+firstButton = Button(topFrame, text="Click here", fg = "green")
+secondButton = Button(topFrame, text="Click here", fg = "red")
+thirdButton = Button(topFrame, text="Click here", fg = "blue")
+fourthButton = Button(bottomFrame, text="Click here", fg = "yellow")
+
+#by default things get packed on top of each other
+firstButton.pack(side = LEFT)
+secondButton.pack(side = LEFT)
+thirdButton.pack(side = LEFT)
+fourthButton.pack(side = TOP)
 
 #need mainloop to run the program till the user closes it
 #so it gives us time to actually view it
 root.mainloop()
+
