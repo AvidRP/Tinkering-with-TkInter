c0481519fa623683442b498cfbd716c57f6f6791
AvidRP <s2pant@edu.uwaterloo.ca>
2017-08-21 20:14:28 -0600

    Events

--- tkinter.py
+++ tkinter.py
@@ -1,28 +1,19 @@
 from Tkinter import *
 
-#using tkinter grid instead of just the pack func
+#mouse click events
 
 root = Tk()
 
-theLabel = Label(root, text="Username")
-theEntry = Entry(root, text="Enter your username")
+def leftClick(event):
+    print("Left")
 
 
-theLabel2 = Label(root, text="Password")
-theEntry2 = Entry(root, text="Password")
+def rightClick(event):
+    print("Right")
 
-
-#Now using the grid to place items
-#sticky is like text-align
-# it takes the value N E S W for north south east west
-theLabel.grid(row=0, sticky=E)
-theLabel2.grid(row=1, sticky=E)
-theEntry.grid(row=0,column=1)
-theEntry2.grid(row=1, column=1)
-
-checkBox = Checkbutton(root, text="Keep me signed in")
-#so that the checkbox takes two column spaces
-checkBox.grid(columnspan=2)
-
+frame = Frame(root, width = 400, height=400)
+frame.bind("<Button-1>", leftClick)
+frame.bind("<Button>", rightClick)
+frame.pack()
 
 root.mainloop()
