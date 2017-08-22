71b00bbb0e377a4cbd162c5b2df73c4b6fe05e06
AvidRP <s2pant@edu.uwaterloo.ca>
2017-08-21 20:24:26 -0600

    using classes

--- tkinter.py
+++ tkinter.py
@@ -1,19 +1,24 @@
 from Tkinter import *
 
-#mouse click events
+#Using classes
+
+class Buttons:
+    #automatically runs this function
+    def __init__(self, master):
+        frame = Frame(master, width=400, height=400)
+        frame.pack()
+
+        self.printButton = Button(frame, text="Print Message", command=self.printMessage)
+        self.printButton.pack(side=LEFT)
+
+        self.quitButton = Button(frame, text="Quit", command=frame.quit)
+        self.quitButton.pack(side=LEFT)
+
+    def printMessage(self):
+        print("MESSAGE!")
 
 root = Tk()
 
-def leftClick(event):
-    print("Left")
-
-
-def rightClick(event):
-    print("Right")
-
-frame = Frame(root, width = 400, height=400)
-frame.bind("<Button-1>", leftClick)
-frame.bind("<Button>", rightClick)
-frame.pack()
+b=Buttons(root)
 
 root.mainloop()
